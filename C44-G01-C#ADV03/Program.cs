b3a49c20711c1cf41f9b7760e2f1d2b30e5987de
Abdullah Hasan <AbdullahAbdelhady929@gmail.com>
2025-08-15 08:20:24 +0300

    Book Class

--- C44-G01-C#ADV03/Program.cs
+++ C44-G01-C#ADV03/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            
+
         }
+        #region Book Class
+        public class Book
+        {
+            #region Properties
+            public string ISBN { get; set; }
+            public string Title { get; set; }
+            public string[] Authors { get; set; }
+            public DateTime PublicationDate { get; set; }
+            public decimal Price { get; set; }
+            #endregion
+        }
+        #endregion 
     }
 }
+
